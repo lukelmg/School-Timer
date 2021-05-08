63e578e313e4048f2d5173bddb3ecf2018de6c0e
Glitch (schooltimer) <none>
2021-05-08 21:57:09 +0000

    https fixes

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var http = require('http');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
 
-var serve = serveStatic("./");
-
-var server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res);
-  serve(req, res, done);
+// Make sure to put this directly after you define your app
+// dont change if you want http -> https
+// if you want https -> http change !req.secure to req.secure and https to http
+app.set('trust proxy', true); // <- required
+app.use((req, res, next) => {
+  if(!req.secure) return res.redirect('https://' + req.get('host') + req.url);
+  next();
 });
 
-server.listen(8000);
+app.use(express.static(__dirname));
 
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Demo app listening on ' + listener.address().port);
+});
+
+
+const axios = require('axios');
+const fs = require('fs');
 var jsonString;
 
 (function emit() {
