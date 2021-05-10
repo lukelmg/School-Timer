08c4e2c17e5d6d7bff941fd4f1d465731019adab
Glitch (schooltimer) <none>
2021-05-10 23:44:19 +0000

    asdfasdf

--- other/sounds.js
+++ other/sounds.js
@@ -28,3 +28,103 @@ function higherSound() {
   var audio = new Audio('sounds/higher.mp3');
   audio.play();
 }
+
+function openSoundPanel() {
+  document.getElementById('soundDiv').style.left = '50%';
+  document.getElementById('everythingElse').style.filter = 'blur(2px)';
+  uisound();
+}
+
+function closeSoundPanel() {
+  document.getElementById('soundDiv').style.left = '-50%';
+  document.getElementById('everythingElse').style.filter = 'blur(0px)';
+  uisound();
+}
+
+
+function openStuffPanel() {
+  document.getElementById("moreStuff").style.left = "50%";
+  document.getElementById("everythingElse").style.filter = "blur(2px)";
+  uisound();
+}
+
+function closeStuffPanel() {
+  document.getElementById("moreStuff").style.left = "-50%";
+  document.getElementById("everythingElse").style.filter = "blur(0px)";
+  uisound();
+}
+
+function requestFeature() {
+  var feature;
+  var type = prompt(
+    "Enter the feature you'd like to see in the next update of schooltimer!",
+    ""
+  );
+  if (type == null || type == "") {
+    //txt = "User cancelled the prompt.";
+  } else {
+    feature = type;
+
+    var xhr = new XMLHttpRequest();
+    var myURL =
+      "https://maker.ifttt.com/trigger/feature/with/key/bc48cOTCsHu3gBrt1nX1EE?value1=" +
+      feature;
+    xhr.open("POST", myURL, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(
+      JSON.stringify({
+        value: "value"
+      })
+    );
+  }
+}
+
+function bugReport() {
+  var feature;
+  var type = prompt(
+    "Enter the bug you experienced on School Timer so that I can fix it.",
+    ""
+  );
+  if (type == null || type == "") {
+    //txt = "User cancelled the prompt.";
+  } else {
+    feature = type;
+    var xhr = new XMLHttpRequest();
+    var myURL =
+      "https://maker.ifttt.com/trigger/bug/with/key/bc48cOTCsHu3gBrt1nX1EE?value1=" +
+      feature;
+    xhr.open("POST", myURL, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(
+      JSON.stringify({
+        value: "value"
+      })
+    );
+  }
+}
+
+var refreshCount = 0;
+
+function readJsonData() {
+  refreshCount++;
+  if (refreshCount <= 6) {
+  document.getElementById("cycleDayOutput").innerHTML = '-';
+  
+  fetch("cycle.json", { cache: "reload" })
+    .then(response => response.json())
+    .then(data => {
+      if (data.today.slice(-1) == 'E') {
+        setNew('E');
+      }
+      document.getElementById("cycleDayOutput").innerHTML = data.today;
+
+      document.getElementById("cycleDayToday").innerHTML = data.today;
+      document.getElementById("cycleDayTomorrow").innerHTML = data.tomorrow;
+      document.getElementById("cycleDayNextDay").innerHTML = data.nextDay;
+    });
+  } else {
+    alert('you are sending too many requests. stop it. \n \nrefresh your browser if you would like to see an updated cycle day')
+  }
+}
+
+readJsonData();
