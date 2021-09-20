d29820081aa45f1ec4c66696e7c6448aadd8f5fa
Krushil Amrutiya <krushilamrutiya@gmail.com>
2021-09-19 23:23:17 -0400

    Update readCycle.js

--- other/readCycle.js
+++ other/readCycle.js
@@ -1,10 +1,8 @@
 function readJsonData() {
-  fetch("https://school-timer-design.vercel.app/cycle.json", { cache: "reload" })
+  fetch("https://613aad1a110e000017a45364.mockapi.io/dayCycle", { cache: "reload" })
     .then(response => response.json())
     .then(data => {
-      if (data.today.slice(-1) == 'E') {
-        setNew('E');
-      }
+      
       console.log(data);
       document.getElementById("cycleDayOutput").innerHTML = data.today;
 
@@ -14,7 +12,7 @@ function readJsonData() {
 
     //  document.getElementById('lastUpdated').innerHTML = 'Last Updated: ' + curHour + ':' + curMin;
     });
-  setTimeout(readJsonData, 60*1000);
+  setTimeout(readJsonData, 60*12*60*1000);
 }
 
 readJsonData();
