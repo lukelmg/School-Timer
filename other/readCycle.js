cc5ebf19ab5efafd0ecd098cd292b2c2daaaad72
Krushil Amrutiya <krushilamrutiya@gmail.com>
2021-09-24 10:43:27 -0400

    Update readCycle.js

--- other/readCycle.js
+++ other/readCycle.js
@@ -1,18 +1,26 @@
+function getDate() //gets the date
+{
+  let  today = new Date();
+  let  dd = String(today.getDate()).padStart(2, '0');
+  let  mm = String(today.getMonth() + 1).padStart(2, '0');
+  let  yyyy     = today.getFullYear();
+  return mm + '-' + dd + '-' + yyyy;
+}
+
 function readJsonData() {
-  fetch("https://613aad1a110e000017a45364.mockapi.io/dayCycle", { cache: "reload" })
+  date = getDate(); //gets the date from the function from above
+  console.log(date)
+  
+  fetch("https://613aad1a110e000017a45364.mockapi.io/dayCycleBeta/" + date, { cache: "reload" })
+
     .then(response => response.json())
     .then(data => {
-      
-      console.log(data);
-      document.getElementById("cycleDayOutput").innerHTML = data.today;
 
+      document.getElementById("cycleDayOutput").innerHTML = data.today; 
       document.getElementById("cycleDayToday").innerHTML = data.today;
       document.getElementById("cycleDayTomorrow").innerHTML = data.tomorrow;
       document.getElementById("cycleDayNextDay").innerHTML = data.nextDay;
-
-    //  document.getElementById('lastUpdated').innerHTML = 'Last Updated: ' + curHour + ':' + curMin;
     });
   setTimeout(readJsonData, 60*12*60*1000);
 }
-
 readJsonData();
